bce9b4727a5f61f2eccd5a0125d694c7f1097bd5
otavio25 <souzaotavio721@gmail.com>
2023-09-07 17:12:01 -0300

    traçando as métricas

--- backend/src/router/data_load_router.js
+++ backend/src/router/data_load_router.js
@@ -3,5 +3,7 @@ const router = express('router')
 const data_load_controller = require('../controller/data_load_controller')
 
 router.post("/dataload", data_load_controller.data_load)
+router.post("update/data", data_load_controller.update_data)
+router.get("/metrics", data_load_controller.get_metrics_papers_aws)
 
 module.exports = router
